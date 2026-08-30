9f5e2239ce440cdd35ff409c751e04b40c9985fe
chayanon <chayanon@chayanon.local>
2026-08-30 13:42:25 +0700

    Assignment04

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const hospitals =require('./routes/hospitals');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
 const app=express();
 
-app.get('/', (req, res) => {
-    res.status (200).json({success:true, data:{id:1}});
-});
-
+app.use('/api/v1/hospitals',hospitals);
 
 const PORT=process.env.PORT || 5003;
 app.listen(PORT,console.log('Server running in ', process.env.NODE_ENV,' mode on port ',PORT));
