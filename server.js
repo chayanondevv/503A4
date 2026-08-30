fe71a8fc3e4aed1012f51d7efc559293021e6cdd
chayanon <chayanon@chayanon.local>
2026-08-30 13:58:14 +0700

    Update PORT

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ const app=express();
 
 app.use('/api/v1/hospitals',hospitals);
 
-const PORT=process.env.PORT || 5003;
+const PORT=process.env.PORT || 5000;
 app.listen(PORT,console.log('Server running in ', process.env.NODE_ENV,' mode on port ',PORT));
